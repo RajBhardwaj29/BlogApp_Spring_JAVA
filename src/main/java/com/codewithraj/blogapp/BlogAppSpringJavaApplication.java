3d31784d83a45178c78f83f7d42201112cc592b6
RajBhardwaj29 <rajbhartendu7@gmail.com>
2023-12-04 15:42:17 +0530

    Initial project setup

---
+++ src/main/java/com/codewithraj/blogapp/BlogAppSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.codewithraj.blogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppSpringJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAppSpringJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithraj/blogapp/BlogAppSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithraj.blogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppSpringJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
